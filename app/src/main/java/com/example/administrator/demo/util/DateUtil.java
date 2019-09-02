53ee6b19833dfd89385b2f35ce266b25a2e5ccf2
tanglonghui <1776234240@qq.com>
2019-09-02 16:11:24 +0800

    时间工具类更新

--- app/src/main/java/com/example/administrator/demo/util/DateUtil.java
+++ app/src/main/java/com/example/administrator/demo/util/DateUtil.java
@@ -1,7 +1,5 @@
 package com.example.administrator.demo.util;
 
-import android.text.format.DateUtils;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,13 +12,19 @@ import java.util.Date;
 public class DateUtil {
     //按需求增加静态实例
     private static final SimpleDateFormat ymdFormat1 = new SimpleDateFormat("yyyy-MM-dd");
-    private static final SimpleDateFormat ymdFormat2 = new SimpleDateFormat("yyyy/M/dd");
-
+    private static final SimpleDateFormat ymdFormat2 = new SimpleDateFormat("yyyy/M/d");
+    private static final SimpleDateFormat ymdFormat3 = new SimpleDateFormat("yyyy年MM月dd日");
+    private static final SimpleDateFormat ymdFormat4 = new SimpleDateFormat("yyyy年M月d日");
+    private static final SimpleDateFormat hmFormat1 = new SimpleDateFormat("HH:mm");
+    private static final SimpleDateFormat mdFormat1 = new SimpleDateFormat("MM-dd");
+    private static final SimpleDateFormat mdFormat2 = new SimpleDateFormat("M月d日");
+    private static final SimpleDateFormat yMdHmsFormat1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat hmsFormat1 = new SimpleDateFormat("HH:mm:ss");
     /**
      * @Description: TODO 指定时间格式的枚举
      */
     public enum Format {
-        ymdFormat1, ymdFormat2
+        ymdFormat1, ymdFormat2,ymdFormat3,hmFormat1,mdFormat1,yMdHmsFormat1,ymdFormat4,mdFormat2,hmsFormat1
     }
 
     /**
@@ -34,6 +38,20 @@ public class DateUtil {
                 return ymdFormat1;
             case ymdFormat2:
                 return ymdFormat2;
+            case ymdFormat3:
+                return ymdFormat3;
+            case hmFormat1:
+                return hmFormat1;
+            case mdFormat1:
+                return mdFormat1;
+            case yMdHmsFormat1:
+                return yMdHmsFormat1;
+            case ymdFormat4:
+                return ymdFormat4;
+            case mdFormat2:
+                return mdFormat2;
+            case hmsFormat1:
+                return hmsFormat1;
         }
         return null;
     }
@@ -71,57 +89,10 @@ public class DateUtil {
     public static String stampTransform(long dateStamp, Format formatAfter) {
         return getSimpleDateFormat(formatAfter).format(new Date(dateStamp));
     }
-// 当我发现Date方法也是比较时间戳之后，炸裂
-//    /**
-//     * @Description: TODO 比较时间
-//     * @return: boolean
-//     * @param: {@link Format}
-//     */
-//    public static boolean isDateBefore(String dateStr1, Format format1, String dateStr2, Format format2) {
-//        Date date1=null, date2=null;
-//        try {
-//            date1 = getSimpleDateFormat(format1).parse(dateStr1);
-//            date2 = getSimpleDateFormat(format2).parse(dateStr2);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        return date1.before(date2);
-//    }
-//    /**
-//     * @Description: TODO 比较时间
-//     * @return: boolean
-//     * @param: {@link Format}
-//     */
-//    public static boolean isDateAfter(String dateStr1, Format format1, String dateStr2, Format format2) {
-//        Date date1=null, date2=null;
-//        try {
-//            date1 = getSimpleDateFormat(format1).parse(dateStr1);
-//            date2 = getSimpleDateFormat(format2).parse(dateStr2);
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//        return date1.after(date2);
-//    }
-//    /**
-//     * @Description: TODO 比较时间
-//     * @return: boolean
-//     * @param: {@link Format}
-//     */
-//    public static boolean isDateBefore(long date1, long date2) {
-//        return  new Date(date1).before(new Date(date2));
-//    }
-//    /**
-//     * @Description: TODO 比较时间
-//     * @return:boolean
-//     * @param: {@link Format}
-//     */
-//    public static boolean isDateAfter(long date1, long date2) {
-//        return  new Date(date1).after(new Date(date2));
-//    }
 
     /**
      * @Description: TODO 将时间（字符串）转换为时间戳(long)
-     * @return: 转换后的时间 String
+     * @return: 转换后的时间 long
      * @param: {@link Format}
      */
     public static long getStamp(String date, Format formatAfter) {
@@ -133,4 +104,8 @@ public class DateUtil {
         }
         return result;
     }
+
+
+
+
 }
