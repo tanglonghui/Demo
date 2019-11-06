46b4aaa14c54219e40e98609bc6bd049aeeea803
tanglonghui <1776234240@qq.com>
2019-11-06 16:12:33 +0800

    some test

---
+++ app/src/main/java/com/example/administrator/demo/video/AndroidVideoActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.demo.video;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.demo.R;
+
+/**
+ * @author TangLongHui
+ * @data created on 2019/11/6
+ * @describe 安卓系统本身的播放器
+ */
+
+public class AndroidVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_video);
+    }
+}
