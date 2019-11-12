3c4c2f3ac815da4094a8c98e3e68c078467f7092
tanglonghui <1776234240@qq.com>
2019-11-12 17:14:07 +0800

    123

---
+++ app/src/main/java/com/example/administrator/demo/viewDraw/VeiwDrawActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.demo.viewDraw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.demo.R;
+
+public class VeiwDrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veiw_draw);
+    }
+}
