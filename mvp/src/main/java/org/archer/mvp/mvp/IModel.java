9baf9a37acade2c234f79ff8a03968e1fa6710e5
tanglonghui <1776234240@qq.com>
2019-12-18 17:07:20 +0800

    一些小东西

---
+++ mvp/src/main/java/org/archer/mvp/mvp/IModel.java
@@ -0,0 +1,8 @@
+package org.archer.mvp.mvp;
+
+/**
+ * Created by Administrator on 2019/12/12.
+ */
+
+public interface IModel {
+}
---
+++ mvp/src/main/java/org/archer/mvp/mvp/IView.java
@@ -0,0 +1,9 @@
+package org.archer.mvp.mvp;
+
+/**
+ * Created by Administrator on 2019/12/12.
+ */
+
+public interface IView {
+    void initData();
+}
