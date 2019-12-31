e1b57bbff5da22f4cdd500f91567cafe7d431727
tanglonghui <1776234240@qq.com>
2019-12-31 17:28:44 +0800

    自定义 view

---
+++ app/src/main/java/com/example/administrator/demo/viewDraw/MyTextView.java
@@ -0,0 +1,34 @@
+package com.example.administrator.demo.viewDraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2019/12/31.
+ */
+
+public class MyTextView extends android.support.v7.widget.AppCompatTextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
